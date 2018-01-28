9ffb9b6ac831c020e9accdd5499f5f4fb885cc86
Rainerino <albertyanyy@gmail.com>
2018-01-28 12:04:41 +0800

    -added git lol set up

---
+++ git_lol.sh
@@ -0,0 +1,6 @@
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+git config --global color.grep auto
+git config --global alias.lol "log --graph --oneline --decorate --color --all"
